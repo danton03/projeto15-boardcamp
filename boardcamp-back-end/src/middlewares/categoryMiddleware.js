190b128335127ebc71341345236eef558df69da3
Danton Matheus <dantoncosta03@hotmail.com>
2022-07-23 20:22:33 -0300

    Corrige a verificação do nome da categoria

--- boardcamp-back-end/src/middlewares/categoryMiddleware.js
+++ boardcamp-back-end/src/middlewares/categoryMiddleware.js
@@ -2,14 +2,14 @@ import connection from "../database/database.js";
 import { categorySchema } from "../schemas/categorySchema.js";
 
 async function checkNameConflicts(req, res, next) {
-  const name = req.body.name[0].toUpperCase() + req.body.name.slice(1).toLowerCase();
   const categoryName = req.body;
   const validateCategory = categorySchema.validate(categoryName);
   
   if(validateCategory.error){ 
-    return res.status(400);
+    return res.sendStatus(400);
   }
 
+  const name = req.body.name[0].toUpperCase() + req.body.name.slice(1).toLowerCase();
   const categoryExists = await connection.query(`
     SELECT * FROM categories 
     WHERE categories.name = $1
@@ -17,7 +17,7 @@ async function checkNameConflicts(req, res, next) {
   );
   
   if(categoryExists.rows.length > 0){
-    return res.status(409);
+    return res.sendStatus(409);
   }
 
   res.locals.categoryName = name;
